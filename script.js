ccbf39b2783c3d5831c975e37653ea79427d531a
kingdomCome <40040615+kingdomcome82@users.noreply.github.com>
2018-07-01 21:19:18 +0100

    updated script with limits

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ function displayPi() {
   var decimalPlaces = document.querySelector('input');
   var displayParagraph = document.querySelector('p');
 
+if (decimalPlaces.value < 0 | decimalPlaces.value > 40 | decimalPlaces.value === "") {
+  displayParagraph.innerHTML = "Please enter a number between 0 and 40";
+} else
   displayParagraph.innerHTML = Number(Math.PI).toFixed(decimalPlaces.value); }
